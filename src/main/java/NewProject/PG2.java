ee43f55a2e34c3ccc1903e7f416548525472b0eb
zlezreg <zlezreg@20193588-MAFES.prod.altengroup.dir>
2020-09-20 13:22:11 +0100

    first classe selenium

---
+++ src/main/java/NewProject/PG2.java
@@ -0,0 +1,20 @@
+package NewProject;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class PG2 {
+    public static void main(String[] args) {
+    	System.setProperty("webdriver.chrome.driver","C://driver/chromedriver.exe");
+    	WebDriver driver = new ChromeDriver();
+        String baseUrl = "http://www.facebook.com";
+        String tagName = "";
+        
+        driver.get(baseUrl);
+        tagName = driver.findElement(By.id("email")).getTagName();
+        System.out.println(tagName);
+        driver.close();
+        System.exit(0);
+}
+}
